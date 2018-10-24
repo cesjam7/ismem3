4bf013f93e6560f71426920b5ab0a2280fe2059e
cesar aquino <cesar7492@gmail.com>
2018-10-23 22:06:07 -0500

    agregue gps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,9 +9,10 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class HomePage {
 
 	public base64Image : string;
+  public fotos : any;
 
   constructor(public navCtrl: NavController, private camera : Camera) {
-
+    this.fotos = [];
   }
 
   tomarFoto() {
@@ -19,13 +20,18 @@ export class HomePage {
   	const options : CameraOptions = {
   		destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
+        mediaType: this.camera.MediaType.PICTURE,
+        quality:100,
+        allowEdit:true,
+        saveToPhotoAlbum:true,
+        cameraDirection:1
   	}
   	this.camera.getPicture(options).then((imageData) => {
-	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
-	}, (err) => {
-		console.log(err)
-	});
+  	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.fotos.push(this.base64Image);
+  	}, (err) => {
+  		console.log(err)
+  	});
 
   }
 
